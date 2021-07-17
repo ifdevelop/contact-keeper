bdbb5a4a8114db15f4dd5bd0fd2fc67c7bbaef92
ifdevelop <if.dev402@gmail.com>
2021-07-17 08:58:56 +0300

    Prepare for Deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,9 +10,13 @@ connectDB();
 // Init Middleware
 app.use(express.json({extended: false}));
 
-app.get('/', (req, res) => res.json({
-    msg: 'Welcome to ContactKeeper API'})
-);
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
