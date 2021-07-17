9e1383ab4f47c559802ea51e4b3aeff9d8808cec
ifdevelop <if.dev402@gmail.com>
2021-07-17 09:31:29 +0300

    Changed Production Routes

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ connectDB();
 // Init Middleware
 app.use(express.json({extended: false}));
 
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
 // Serve static assets in production
 if(process.env.NODE_ENV === 'production') {
     // Set static folder
@@ -17,11 +22,6 @@ if(process.env.NODE_ENV === 'production') {
 
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
-
-// Define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
